7cacfdb8ad48132b1ab63739e55b666778a72e31
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-06-13 20:25:43 +0800

    start compiler journey

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
-#include <cstdio>
-#include "chain.h"
+#include "common.h"
 
-int main() {
-  Node* head = new Node{nullptr, 1, nullptr};
-
-  insert(3, head, head);
-  insert(5, head->next, head);
-
-  Node* insert_pos = find(1, head);
-  insert(4, insert_pos, head);
-
-  Node* print_ptr = head;
-  while (print_ptr != nullptr)
-  {
-    printf("%p address is %d\n", print_ptr, print_ptr->data);
-    print_ptr = print_ptr->next;
-  }
-  
-  return 0;
+int  main ( int  argc , const  char * argv []) {
+   return  0 ;
 }
