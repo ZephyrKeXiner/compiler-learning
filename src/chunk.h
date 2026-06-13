20ed1dde5528f2350591df6b2b47090b74e91dd0
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-06-13 20:40:48 +0800

    add memory

--- src/chunk.h
+++ src/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "memory.h"
 
 typedef enum {
   OP_RETURN,
@@ -9,8 +10,25 @@ typedef enum {
 
 typedef struct {
   int count;
-  int capability;
+  int capacity;
   uint8_t* code;
 } Chunk;
+
+void initChunk(Chunk* chunk) {
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = nullptr;
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+  if (chunk->capacity < chunk->count + 1) 
+  {
+    int oldCapacity = chunk->capacity;
+    int newCapacity = GROW_CAPACITY(oldCapacity);
+    
+  }
+  chunk->count += 1;
+  chunk->code[chunk->count] = byte;
+}
 
 #endif
