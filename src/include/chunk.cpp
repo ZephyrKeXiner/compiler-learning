76eaf725fbfc6efeb861fb5158d827d757e4da73
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-06-14 14:39:50 +0800

    add line information

--- src/include/chunk.cpp
+++ src/include/chunk.cpp
@@ -5,18 +5,22 @@ void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = nullptr;
+  chunk->lines = nullptr;
   initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   if (chunk->capacity < chunk->count + 1) 
   {
     int oldCapacity = chunk->capacity;
     chunk->capacity = growCapacity(oldCapacity);
     chunk->code = growArray<uint8_t>(chunk->code, 
                     oldCapacity, chunk->capacity);
+    chunk->lines = growArray<int>(chunk->lines,
+                    oldCapacity, chunk->capacity);
   }
   chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line;
   chunk->count++;
 }
 
@@ -27,6 +31,7 @@ int addConstant(Chunk* chunk, Value value) {
 
 void freeChunk(Chunk* chunk) {
   freeArray<uint8_t>(chunk->code, chunk->capacity);
+  freeArray<int>(chunk->lines, chunk->capacity);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
