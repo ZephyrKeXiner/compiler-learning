e70c1be4557c5e7c62eba79ef156d37f807d10a5
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-06-14 10:46:30 +0800

    use template and constexpr

--- src/include/chunk.cpp
+++ src/include/chunk.cpp
@@ -11,8 +11,8 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
   if (chunk->capacity < chunk->count + 1) 
   {
     int oldCapacity = chunk->capacity;
-    chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code = GROW_ARRAY(uint8_t, chunk->code, 
+    chunk->capacity = growCapacity(oldCapacity);
+    chunk->code = growArray<uint8_t>(chunk->code, 
                     oldCapacity, chunk->capacity);
   }
   chunk->code[chunk->count] = byte;
@@ -20,6 +20,6 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
 }
 
 void freeChunk(Chunk* chunk) {
-  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeArray<uint8_t>(chunk->code, chunk->capacity);
   initChunk(chunk);
 }
--- src/include/memory.h
+++ src/include/memory.h
@@ -5,6 +5,24 @@
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
+constexpr int growCapacity(int capacity) {
+  return (capacity < 8) ? 8 : capacity * 2;
+}
+
+template <typename T>
+T* growArray(T* pointer, size_t oldSize, size_t newSize){
+  return static_cast<T*> (
+    reallocate(pointer, sizeof(T) * oldSize, sizeof(T) * newSize)
+  );
+}
+
+template <typename T>
+T* freeArray(T* pointer, size_t oldSize) {
+  return static_cast<T*> (
+    reallocate(pointer, sizeof(T) * oldSize, 0)
+  );
+}
+
 #define GROW_CAPACITY(capacity) \
   ((capacity < 8) ? 8 : capacity * 2)
 
